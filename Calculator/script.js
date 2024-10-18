c133c4015d18aec39f77144e25236a491e22d50d
Intakhab2004 <ikbalam4648@gmail.com>
2024-10-18 19:05:20 +0530

    Modify Calculator

--- Calculator/script.js
+++ Calculator/script.js
@@ -4,14 +4,21 @@ let buttons = document.querySelectorAll(".btn");
 Array.from(buttons).forEach((button) => {
     button.addEventListener("click", (e) => {
         if(e.target.innerHTML == "="){
-            string = eval(string);
-            document.querySelector(".output").value = string;
+            try{
+                let temp = string.replace(/x/g, "*").replace(/\^/g, "**");
+                string = eval(temp);
+                document.querySelector(".output").value = string;
+            }
+            catch(error){
+                document.querySelector(".output").value = "Error";
+            }
         }
 
         else if(e.target.innerHTML == "AC"){
             string = "";
             document.querySelector(".output").value = string;
         }
+
         else{
             console.log(e.target);
             string = string + e.target.innerHTML;
